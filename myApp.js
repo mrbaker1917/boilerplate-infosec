d94ad70a56b218cc785485c052eb96a0fe7f2109
Mark R Baker <mrbaker1917@gmail.com>
2021-05-14 14:35:44 -0700

    sets port

--- myApp.js
+++ myApp.js
@@ -55,6 +55,12 @@ app.use('/_api', api);
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
+// var listener = app.listen(process.env.PORT || 3000, function () {
+//   console.log('Your app is listening on port ' + listener.address().port);
+// });
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 8000;
+}
+app.listen(port);
